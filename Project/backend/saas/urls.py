b3fb0997a191b33cea97ce4ff58ac5cb6738f192
Herostere <bossartlaurent@gmail.com>
2023-01-15 18:58:08 +0100

    sign up / sign in / log out

--- Project/backend/saas/urls.py
+++ Project/backend/saas/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('upload/', views.image_upload_view, name='upload'),
 ]
--- Project/backend/saas/views.py
+++ Project/backend/saas/views.py
@@ -1,5 +1,18 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from .forms import ImageForm
 
 
 def index(request):
-    return HttpResponse("Hello, friend. It is the main page.")
+    return render(request, "base.html")
+
+
+def image_upload_view(request):
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            img_obj = form.instance
+            return render(request, 'upload.html', {'form': form, 'img_obj': img_obj})
+    else:
+        form = ImageForm()
+    return render(request, 'upload.html', {'form': form})
