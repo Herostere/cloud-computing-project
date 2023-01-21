da5b204ac029fa16bcce941c8077d31c71d5e03c
Herostere <bossartlaurent@gmail.com>
2023-01-21 14:52:23 +0100

    frontend

--- Project/backend/accounts/views.py
+++ Project/backend/accounts/views.py
@@ -6,7 +6,7 @@ from django.views import generic
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy("account/login")
+    success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
 
     def dispatch(self, request, *args, **kwargs):
